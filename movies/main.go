5d36caf0dd3e37b7075d58c66661458b8368709f
aeshes <mathesis@inbox.ru>
2018-05-19 22:50:02 +0300

    add Meta for HTTP meta

--- movies/main.go
+++ movies/main.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"movies/dao"
 	"movies/models"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"gopkg.in/mgo.v2/bson"
 )
 
 var d = dao.MoviesDAO{}
+
+func TestingEndpoint(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	fmt.Println(r.Header["Content-Range"])
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+	}
+	file, err := os.OpenFile("tmp.jpg", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := file.Write(data); err != nil {
+		log.Fatal(err)
+	}
+	if err := file.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
 
 // AllMoviesEndPoint show all movies
 func AllMoviesEndPoint(w http.ResponseWriter, r *http.Request) {
@@ -77,6 +99,7 @@ func main() {
 	router.HandleFunc("/movies", AllMoviesEndPoint).Methods("GET")
 	router.HandleFunc("/movies", CreateMoviesEndPoint).Methods("POST")
 	router.HandleFunc("/movies", UpdateMovieEndPoint).Methods("PUT")
+	router.HandleFunc("/testing", TestingEndpoint).Methods("PUT")
 
 	if err := http.ListenAndServe(":3000", router); err != nil {
 		log.Fatal(err)
